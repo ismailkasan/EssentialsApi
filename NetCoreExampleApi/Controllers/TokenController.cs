f26087031a332e3e268c566c56ba0bcdc1478289
ismailkasan <ismail.kasan@sirenbilisim.com.tr>
2023-08-12 11:35:06 +0300

    deneme

--- NetCoreExampleApi/Controllers/TokenController.cs
+++ NetCoreExampleApi/Controllers/TokenController.cs
@@ -89,7 +89,7 @@ namespace NetCoreExampleApi.Controllers
         [Route("Test")]
         public IActionResult Test()
         {
-            return Ok("Test başarılı linux");
+            return Ok("Test başarılı linux github");
         }
     }
 }
